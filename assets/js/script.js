488fdf2fac3b4f1ffdbb08683aaf15ecb4499442
jacobsousa <jacobsousa92@gmail.com>
2022-04-26 16:21:27 -0700

    initial setup

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+
+//document opens to reveal invitation to take the quiz
+
+//eventlistener for click on start button
+
+//startbutton will activate 60 sec count down timer 
+
+//the question(s) will appear with multiple choice answers
+
+//if statements for right & wrong answers 
+
+//time subtracted for wrong answers 
+
+//timer reaches zero or all questions answered game ends
+
+//player can add initials & save to local storage
