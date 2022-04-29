a98cbe54cc57d9616ba159ec7130ebfbb62c1d5a
jacobsousa <jacobsousa92@gmail.com>
2022-04-29 10:29:38 -0700

    startbtn answerbtn listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,58 @@
+var Questions=[
+    {
+        question:"What color is the sky?",
+        choices:["Green","Blue","Red","Yellow"],
+        answer:"Blue"
+    },
+    {
+        question:"What comes after three?",
+        choices:["one","two","five","four"],
+        answer:"four"
+    },
+    {
+        question:"What animal goes Moo?",
+        choices:["dog","cow","bird","cat"],
+        answer:"cow"
+    },
+    {
+        question:"Which is sweet?",
+        choices:["salt","sand","sardines","salt water taffy"],
+        answer:"salt water taffy"
+    },
+]
 
-
-//document opens to reveal invitation to take the quiz
+var currentQuestion=0
 
 //eventlistener for click on start button
+document.querySelector(".startbtn").addEventListener("click", function(){
+    document.querySelector(".quizArea").style.display="block"
+    document.querySelector(".startArea").style.display="none"
+    document.querySelector(".question").innerText=Questions[currentQuestion].question
+
+for(i=1; i<5; i++){
+    document.querySelector(".answerbtn"+i).innerText=Questions[currentQuestion].choices[i-1]
+    console.log(i)
+    // Question and buttons will be updated with current question
+
+}
+})
+
+for(i=1; i<5; i++){
+    document.querySelector(".answerbtn"+i).addEventListener("click", function(event){
+        console.log(event)
+        // 
+    })
+
+}
 
 //startbutton will activate 60 sec count down timer 
+
+//startbutton will hide after clicked
 
 //the question(s) will appear with multiple choice answers
 
 //if statements for right & wrong answers 
+
 
 //time subtracted for wrong answers 
 
